ca9185aac3bad428767664d78d7fb0024288bc29
desh04 <singh.desh0404@gmail.com>
2021-08-21 23:10:37 +0530

    rewritten render funtion, added start and finish funtion

--- Components/function-method.js
+++ Components/function-method.js
@@ -1,8 +1,16 @@
+
+/* let startBtn = document.createElement('button');
+let finishBtn = document.createElement('button');
+let delBtn = document.createElement('button');
+
+const btnArr = [startBtn , finishBtn, delBtn] ; */
+
 export function addToDo(input, arr) {
   const taskElement = { task: input, completed: false, id: Date.now() };
 
-  arr.push(taskElement);
+  arr.unshift(taskElement);
   console.log(arr);
+  
   renderToDo(taskElement);
 }
 
@@ -10,26 +18,50 @@ export function renderToDo(todo) {
   const list = document.querySelector(".task-list");
 
   const node = document.createElement("li");
+  const startBtn = document.createElement("button");
+  const finishBtn = document.createElement("button");
+  const delBtn = document.createElement("button");
 
   node.setAttribute("class", "toDoItem");
   node.setAttribute("data-key", todo.id);
+  node.innerText = todo.task;
 
-  node.innerHTML = `
-  <input id="${todo.id}" type="checkbox"/>
-  <label for="${todo.id}" class="tick js-tick"></label>
-  <span>${todo.task}</span>
-  <button class="start-todo js-delete-todo">
-      <img src='#' alt='start' width='15px'></img>
-  </button>
-  <button class="Finished-todo js-delete-todo">
-      <img src='#' alt='Finished' width='15px'></img>
-  </button>    
-  <button class="delete-todo js-delete-todo">
-      <img src='./image/del1.png' alt='delete' width='15px'></img>
-  </button>
-  
-`;
+  startBtn.classList.add('start-btn');
+  startBtn.innerHTML= '<i class="fas fa-play"></i>';
+  startBtn.setAttribute("data-key", todo.id);
 
-  //<svg><use href="./image/del1.png"></use></svg>
+  finishBtn.classList.add('finish-btn');
+  finishBtn.innerHTML = '<i class="fas fa-check"></i>';
+  finishBtn.setAttribute("data-key", todo.id);
+
+  delBtn.classList.add('del-btn');
+  delBtn.innerHTML = '<i class="fas fa-trash"></i>'; 
+  delBtn.setAttribute("data-key", todo.id);
+
+  node.appendChild(startBtn);
+  node.appendChild(finishBtn);
+  node.appendChild(delBtn);
+
   list.append(node);
 }
+
+export function startTask(input) {
+  const runningList = document.querySelector('.running-list');
+  
+  console.log(input.parentElement.parentElement);
+  console.log('this is inside the start Function', input.parentElement.parentElement.childNodes[0]);
+
+  runningList.appendChild(input.parentElement.parentElement);
+  input.parentElement.parentElement.removeChild(input.parentElement.parentElement.childNodes[1]);
+}
+
+export function finishTask(input) {
+  const finishedList = document.querySelector('.finished-list');
+  
+  console.log(input.parentElement.parentElement);
+  console.log('this is inside the start Function', input.parentElement.parentElement.childNodes[0]);
+
+  finishedList.appendChild(input.parentElement.parentElement);
+  input.parentElement.parentElement.removeChild(input.parentElement.parentElement.childNodes[1]);
+}
+
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import { addToDo, renderToDo } from "../Components/function-method.js";
+import { addToDo, renderToDo, startTask, finishTask } from "../Components/function-method.js";
+
 
 window.onload = function () {
   document.getElementById("input-field").focus();
@@ -13,7 +14,7 @@ const addBtn = document.querySelector("#add-task-btn");
 
 addBtn.addEventListener("click", function () {
   const taskText = document.querySelector("#input-field");
-  const text = taskText.value.trim();
+  const text = taskText.value.trim();      // remove white spaces from front and back .
 
   if (text != "") {
     addToDo(text, taskArray);
@@ -22,4 +23,27 @@ addBtn.addEventListener("click", function () {
   document.getElementById("input-field").focus();
 });
 
+const container = document.querySelector('.container')
+
+container.addEventListener('click', function(e) {
+  if (e.target.parentElement.classList[0] === 'del-btn') {
+    console.log('Had clicked DEL button', e.target.parentElement.getAttribute('data-key'));
+    // Put Funtion here to Del the task
+  } 
+  else if (e.target.parentElement.classList[0] === 'start-btn') {
+    console.log('Had clicked START button', e.target.parentElement.getAttribute('data-key'));
+    //  Put Function here to move the task to ON GOING section
+    startTask(e.target);
+  } 
+  else if (e.target.parentElement.classList[0] === 'finish-btn') {
+    console.log('Had clicked FINISH button', e.target.parentElement.getAttribute('data-key'));
+    // Put the function here to move the task to move to FINISHED section
+    finishTask(e.target);
+  }
+  console.log(e.target.parentElement);
+})
+
 addToDo("new task has been added", taskArray);
+
+
+
